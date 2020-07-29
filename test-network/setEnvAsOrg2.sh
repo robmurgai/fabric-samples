31cad79e76069f8813105f5219e61276074a3a84
Rob Murgai <murgai@us.ibm.com>
2020-07-28 21:22:38 -0400

    Add debug tools to set up environment variables and monitoring docker

---
+++ test-network/setEnvAsOrg2.sh
@@ -0,0 +1,9 @@
+# Environment variables for Org2
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+echo CORE_PEER_LOCALMSPID = $CORE_PEER_LOCALMSPID
