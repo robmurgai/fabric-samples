2f41d3f745c022ee35bad3d4b90e019866711d69
Rob Murgai <smurgai@gmail.com>
2020-09-18 00:20:36 -0400

    Remove excess printing to terminal in setPeerEnv.sh
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- test-network/setPeerEnv.sh
+++ test-network/setPeerEnv.sh
@@ -5,9 +5,6 @@ source scriptUtils.sh
 # import utils
 . scripts/envVar.sh
 
-## Set Variables for Org 
-infoln "Setting Environment Varibales for Org ${MODE}"
-
 # Print the usage message
 function printHelp() {
   println "Usage: "
@@ -19,14 +16,12 @@ function printHelp() {
 ## Parse mode
 if [[ $# -lt 1 ]] ; then
   printHelp
-  exit 0
 else
   MODE=$1
 fi
 
-set -x
+#set -x
 export PATH=${PWD}/bin:$PATH
 export FABRIC_CFG_PATH=$PWD/config/
 setGlobals "${MODE}" 
 
-exit 0
